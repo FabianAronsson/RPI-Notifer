81ab7a161ff94361f3167b5d646e4c2b23d2e4d4
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-01 17:13:32 +0200

    Add experimental github client with octokit

--- RPI-Notifier/Program.cs
+++ RPI-Notifier/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Octokit;
+
+ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+IConfiguration configuration = configurationBuilder.AddUserSecrets<Program>().Build();
+string githubToken = configuration.GetSection("secret")["supersecret"];
+
 Console.WriteLine("Hello, World!");
+
+var client = new GitHubClient(new ProductHeaderValue("testApp"))
+{
+    Credentials = new Credentials(githubToken)
+};
+var user = await client.Issue.GetAllForCurrent();
+Console.WriteLine(user.Count);
