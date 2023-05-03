cbc7302793f3f9f0e3291e09e8b433417f1e6eee
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-03 08:28:14 +0200

    Add Dependency Injection

--- RPI-Notifier/Program.cs
+++ RPI-Notifier/Program.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Octokit;
+
+
+using IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureServices(services =>
+	{
+
+	})
+	.Build();
+
+
+await host.RunAsync();
 
 ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 IConfiguration configuration = configurationBuilder.AddUserSecrets<Program>().Build();
 string githubToken = configuration.GetSection("secret")["supersecret"];
 
-Console.WriteLine("Hello, World!");
 
 var client = new GitHubClient(new ProductHeaderValue("testApp"))
 {
-    Credentials = new Credentials(githubToken)
+	Credentials = new Credentials(githubToken)
 };
 var user = await client.Issue.GetAllForCurrent();
 Console.WriteLine(user.Count);
+
